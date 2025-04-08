97fe1567013c00a8ca1874b0dbca2391d3aebde6
xMentory <bennibrovoldcr@gmail.com>
2025-04-08 23:39:49 +1000

    feat: up

---
+++ assets/index-C0QoUHtI.js
@@ -0,0 +1,48 @@
+import{j as s,C as t,d as e,m as d}from"./index-C-SMngWq.js";import{H as l}from"./Helmet-U944v3lS.js";const r=e.h2`
+  font-size: 24px;
+  color: #000;
+  margin-bottom: 10px;
+  margin-top: 0px;
+`,n=e.p`
+  font-size: 20px;
+  color: #333;
+  margin-bottom: 0px;
+  font-weight: bold;
+`,c=e.div`
+  margin-bottom: 100px;
+  font-size: 18px;
+
+  p:first-child {
+    margin-top: 0px;
+  }
+`,i=e.span`
+  font-weight: bold;
+  margin-right: 10px;
+`,h=e.div`
+  font-size: 18px;
+  margin-top: 8px;
+`,v=()=>s.jsxs(o,{children:[s.jsxs(l,{children:[s.jsx("title",{children:"Контакты | Металлические конструкции"}),s.jsx("meta",{name:"description",content:"Наша компания находится по адресу... Наши контакты: телефон, email, адрес."}),s.jsx("meta",{name:"keywords",content:"контакты, металлические конструкции, компания"})]}),s.jsxs(t,{children:[s.jsxs(x,{children:[s.jsx(r,{children:"Контакты Акватрэйд"}),s.jsx(n,{children:"Мы работаем для Вас"})]}),s.jsx(n,{children:"Директор"}),s.jsx(h,{children:"Иванов Игорь Юрьевич"}),s.jsxs(c,{children:[s.jsx(n,{children:"ЮРИДИЧЕСКАЯ ИНФОРМАЦИЯ"}),s.jsx("p",{children:"Общество с ограниченной ответственностью «Акватрэйд»"}),s.jsxs("div",{children:[s.jsx(i,{children:"АДРЕС"}),"680000, г. Владивосток, ул. Ленина 18 В"]}),s.jsxs("div",{children:[s.jsx(i,{children:"Идентификационный номер (ИНН)/КПП"}),"2539067070/254001001"]}),s.jsxs("div",{children:[s.jsx(i,{children:"Номер расчетного счета"}),"40702810500050000884"]}),s.jsxs("div",{children:[s.jsx(i,{children:"Номер корреспондентского счета"}),"30101810900000000705"]}),s.jsxs("div",{children:[s.jsx(i,{children:"Полное наименование банка"}),'ПАО "Дальневосточный банк"']}),s.jsxs("div",{children:[s.jsx(i,{children:"Местонахождение банка"}),"690990 Приморский край, г. Владивосток, ул. Верхнепортовая д. 27А"]}),s.jsxs("div",{children:[s.jsx(i,{children:"БИК"}),"040507705"]}),s.jsxs("div",{children:[s.jsx(i,{children:"Код отрасли по ОКАТО"}),"05401372000"]}),s.jsxs("div",{children:[s.jsx(i,{children:"Код организации по ОКПО"}),"744988671"]}),s.jsxs("div",{children:[s.jsx(i,{children:"ОКТМО"}),"05701000001"]}),s.jsxs("div",{children:[s.jsx(i,{children:"ОКВЭД"}),"28.21"]}),s.jsxs("div",{children:[s.jsx(i,{children:"ОГРН"}),"1052504084665"]})]}),s.jsxs(c,{children:[s.jsxs(x,{children:[s.jsx(r,{children:"ГРАФИК РАБОТЫ"}),s.jsx(n,{children:"И адрес офиса"})]}),s.jsxs(p,{children:[s.jsxs(j,{children:[s.jsx(n,{children:"ЧАСЫ РАБОТЫ"}),s.jsxs("div",{children:["Понедельник - пятница ",s.jsx("span",{children:"8:00 – 17:00"})]}),s.jsxs("div",{children:["Суббота - воскресенье ",s.jsx("span",{children:"выходной"})]}),s.jsx(n,{children:"АДРЕС ОФИСА"}),s.jsx("p",{children:"690011 Приморский край, г. Владивосток, ул. Чукотская 6А, пом.2"}),s.jsx(n,{children:"Эл.почта"}),s.jsx("a",{href:"mailto:i707063@yandex.ru",children:"i707063@yandex.ru"}),s.jsx(n,{children:"Телефон"}),s.jsx("a",{href:"tel:+79147907063",children:"+7(914) 790-70-63"})]}),s.jsxs(a,{children:[" ",s.jsx("iframe",{src:"https://yandex.ru/map-widget/v1/?um=constructor%3A84c2291cbd1f208006ceaa92556b1a087151d730631275f2d98478f6c1516e56&source=constructor"})]})]})]})]})]}),o=e.div`
+  padding: 16px;
+`,j=e.div``,a=e.div`
+  iframe {
+    width: 600px;
+    height: 280px;
+
+    ${d.pure.less(d.size.md)} {
+      width: 100%;
+    }
+  }
+`,p=e.div`
+  display: flex;
+  margin-top: 32px;
+  justify-content: space-between;
+
+  ${d.pure.less(d.size.sm)} {
+    flex-direction: column;
+  }
+`,x=e.div`
+  padding: 16px 32px;
+  margin-top: 32px;
+
+  border-left: 5px solid rgb(66, 153, 255);
+`;export{v as default};
